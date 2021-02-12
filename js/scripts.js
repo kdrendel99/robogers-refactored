461e38f3b7d34ecd32636097bbca9166e3151699
Karlson Drendel <kdrendel99@gmail.com>
2021-02-11 16:21:11 -0800

    Build functionality for 'boop' paramaters.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ function beepBoop(input){
   for (i = 0; i <= input; i++) {
     if ((i+'').indexOf('3') > -1){
       console.log("neighbor")
-    }
+    }else if ((i+'').indexOf('2') > -1){
+      console.log("boop")}
     else {console.log(i)}; }
   }
